2446cb3dea0c87518551e63ded322d1bf23cec37
Maximun <rajatvermayt78645@gmail.com>
2026-02-05 22:07:51 +0530

    Added create command

--- Program.cs
+++ Program.cs
@@ -15,8 +15,69 @@ namespace back_end
     {
         public static void Main(string[] args)
         {
+            if (args.Any((arg) => arg.Equals("create"))) {
+                AddUser();
+            }
             WebApplication app = BuildBuilder(WebApplication.CreateBuilder(args));
             RunApplication(app);
+        }
+
+        private static void AddUser()
+        {
+            // Load appsettings.json from API project
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var options = new DbContextOptionsBuilder<DBContext>()
+                .UseSqlServer(config.GetConnectionString("DefaultConnection"))
+                .Options;
+
+            using var db = new DBContext(options);
+
+            Console.Write("Name: ");
+            var name = Console.ReadLine();
+
+            Console.Write("Email: ");
+            var email = Console.ReadLine();
+
+            Console.Write("Password: ");
+            var password = Console.ReadLine();
+
+            Console.Write("Roles (comma seprated): ");
+            var roles = Console.ReadLine();
+
+            if (db.Users.Any(u => u.Email == email))
+            {
+                Console.WriteLine("User already exists!");
+                return;
+            }
+
+            var hasher = new PasswordHasher<UserDB>();
+
+            var admin = new UserDB
+            {
+                Id = Guid.NewGuid(),
+                Email = email!,
+                Name = name!,
+                Roles = roles!,
+            };
+
+            admin.HashedPassword = hasher.HashPassword(admin, password!);
+
+            db.Users.Add(admin);
+            db.SaveChanges();
+
+            Console.WriteLine($"✅ User created successfully with {roles} roles.");
+            IEnumerable<UserDB> users = db.Users;
+            if (users.Any())
+            {
+                Console.WriteLine("Admins:");
+                foreach (UserDB user in users)
+                {
+                    Console.WriteLine($"Name = {user.Name}; Email = {user.Email}; Roles = {user.Roles}; Id = {user.Id}");
+                }
+            }
         }
 
         public static void RunApplication(WebApplication app)
@@ -50,7 +111,7 @@ namespace back_end
                 o.EnableSensitiveDataLogging();
             });
 
-            builder.Services.AddScoped<PasswordHasher<UserDB>>();  
+            builder.Services.AddScoped<PasswordHasher<UserDB>>();
             builder.Services.AddScoped<IStudentService, StudentService>();
             builder.Services.AddScoped<IAuthService, AuthService>();
 
